56523ac84ff87eba3b8418f35501586193d894b7
tegsheeshde <tegsheeshd@gmail.com>
2025-12-03 13:12:10 +0800

    somefixes

--- src/firebase.js
+++ src/firebase.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL || `https://${import.meta.env.VITE_FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL || 'https://permumdota-default-rtdb.asia-southeast1.firebasedatabase.app'
 };
 
 // Initialize Firebase
